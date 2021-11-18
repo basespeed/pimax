dbb31d719fed45560a4c873bb7cbbc7e5901fec7
basespeed <tranthanhtung.it02@gmail.com>
2021-11-18 14:20:20 +0700

    pimax ver1.1

--- pimax.php
+++ pimax.php
@@ -3,7 +3,7 @@
 Plugin Name: PiMax
 Plugin URI: https://pimax.me
 Description: WordPress Plugin Assistants
-Version: 1.2
+Version: 1.1
 Author: Pitagon
 Author URI: https://pitagon.io
 Text Domain: pimax
